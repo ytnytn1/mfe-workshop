1af75be77fa0f8edf54015669b27625c1d65f5e5
ivang-shoreline <ivang@shoreline.io>
2022-05-24 23:30:41 +0700

    add not working app1.1

---
+++ app1.1/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const RemoteButton = React.lazy(() => import('app2/Button'));
+
+const App = () => {
+  const [counter, setCounter] = React.useState(0)
+  return (
+    <div>
+      <h1>Host</h1>
+      <h2>App 1.1</h2>
+      <React.Suspense fallback="Loading Button">
+        <RemoteButton onClick={() => {
+          setCounter(counter + 1)
+        }}
+        >
+          {counter}
+        </RemoteButton>
+      </React.Suspense>
+    </div>
+  );
+};
+
+export default App;
---
+++ app1.1/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+const deps = require('./package.json');
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3003,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  //http://localhost:3002/remoteEntry.js
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app1',
+      remotes: {
+        app2: `app2@http://localhost:3002/remoteEntry.js`,
+      },
+      shared: {
+        react: { singleton: true, eager: true, requiredVersion: deps.dependencies.react },
+        'react-dom': { singleton: true, eager: true, requiredVersion: deps.dependencies['react-dom'] }
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
